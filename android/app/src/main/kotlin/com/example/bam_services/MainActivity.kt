35eb80af76e46715a961bae1a9e899cb1665516d
unknown <rhy159753@gmail.com>
2020-07-28 16:25:19 +0800

    First Push

---
+++ android/app/src/main/kotlin/com/example/bam_services/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bam_services
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/screens/HomeScreen.dart
@@ -0,0 +1,77 @@
+import 'package:bam_services/ui/screens/SearchedSBScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:bam_services/ui/constant/constant.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.bottomLeft,
+        width: deviceWidth(context),
+        color: cBackgroundColor1,
+        padding: EdgeInsets.only(top: 30),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: deviceHeight(context) * 0.11),
+                child: Image(image: AssetImage("assets/logo/bam.png")),
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomLeft,
+              width: deviceWidth(context),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              margin: EdgeInsets.only(top: deviceHeight(context) * 0.17),
+              child: Column(
+                children: [
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    margin:
+                        EdgeInsets.only(bottom: deviceHeight(context) * 0.021),
+                    child: Text(
+                      "No Servis",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      hintText: "SB - XXXXXX",
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    margin: EdgeInsets.only(top: deviceHeight(context) * 0.021),
+                    child: RaisedButton(
+                        color: Colors.black,
+                        textColor: Colors.white,
+                        child: Icon(Icons.search),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return SearchedSBScreen();
+                          }));
+                        }),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
